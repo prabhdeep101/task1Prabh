c1f610b2cd6f69a3b723999a0e14da56eb7b6421
Prabhdeep101 <prabhdeepsachdeva101@gmail.com>
2024-03-28 13:28:26 +1300

    Add project files.

---
+++ task1Prabh/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace task1Prabh;
+
+public class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello World");
+    }
+
+    public static void ProcessNumbers()
+    {
+        int a[2] = GetNumber("Please enter the first set of numbers: ");
+    }
+}
